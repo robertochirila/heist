f44c058c4fcb9077a46eb51a4a893dc5ae2fd1c5
unknown <robertochirila@yahoo.com>
2018-10-23 12:19:56 +0100

    'Responsive'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import Post from './components/Post'
 import { CSSTransition } from 'react-transition-group'
 
+
+/**
+ * Apart from the requirements which I have received I have also added animations and improved the UI a little bit
+ * I have also changed the paddings for some elements a little bit, for example the date, if this 
+ * doesn't look good please tell me and I can modify the paddings
+ * Bellow I have commented my code, thank you very much
+ */
+
 class App extends Component {
 
 
@@ -13,14 +21,19 @@ class App extends Component {
       index:1,
       counter:0
     }
-
+    // A good practice to bind the methods in the constructor
+    // Another good practice would have been to use arrow functions in the render method
     this.rightHandler=this.rightHandler.bind(this);
     this.leftHandler=this.leftHandler.bind(this);
 
   }
 
   componentDidMount(){
-
+    // fetch data from the file
+    // return a promise
+    // convert the response to json
+    // update state
+    // this can be applied to any number of articles
     fetch('response.json')
     .then((resp)=>{ 
       return resp.json() })
@@ -37,6 +50,9 @@ class App extends Component {
 
   rightHandler(){
 
+    // clicking the right arrow triggers this
+    // updates the index by 1 if the index is less than the max number of articles
+
     if(this.state.index!==this.state.counter){
       this.setState({index:this.state.index+1})
     }
@@ -46,7 +62,7 @@ class App extends Component {
   }
 
   leftHandler(){
-
+    // same as the righthandler
     if(this.state.index>1){
       this.setState({index:this.state.index-1})
     }
@@ -56,23 +72,31 @@ class App extends Component {
 
 
   render() {
-
+    // store the posts and index
+    
     const { posts } = this.state;
     const { index } = this.state;
     
 
     return (
       <div className="App">
-      
+      {/*
+      Loop through all the posts
+      Return only the article that matches the index
+      Array indexing starts at 0 while index at 1
+      That is why I use i+1
+      */}
       { posts.map((data,i)=>{
          if(i+1===index)
-         
+         /**
+          * Return the post and animate it
+          */
          return <div key={i}>
            <CSSTransition
            key={i}
            in={true}
            appear={true}
-           timeout={1000}
+           timeout={2000}
            classNames='fade'
            >
            <Post data={data} key={i}/>
@@ -81,10 +105,10 @@ class App extends Component {
          
       }) }
         
-      
+        
         <ul className='App-navigation'>
           <li className='App-navitem'><h4>{this.state.index}/{this.state.counter}</h4></li>
-          <li className='App-navitem'><h3>VIEW ALL POSTS</h3></li>
+          <li className='App-navitem' id='viewAll'><h3>VIEW ALL POSTS</h3></li>
           <li className='App-navitem' onClick={this.leftHandler}><img src='../icons/arrow.svg' id='leftArrow' className='App-arrow'></img></li>
           <li className='App-navitem' onClick={this.rightHandler}><img src='../icons/arrow.svg' className='App-arrow'></img></li>
         </ul>
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,19 +2,32 @@ import React from 'react'
 import '../App.css';
 
 
+/**
+ * 
+ * No need to use a statefull component
+ * No need for lifecycle methods and state
+ */
+
 const Post=(props)=>{
+
+    /**
+     * I have created a date object with the param which I receive from props
+     * I noticed that it return the time +1 day so I have subtracted 1
+     * To get the name of the month instead of the number I have created the array bellow
+     * I then get the index of the month and use the array to get the name
+     */
 
     const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
     ];
-    //console.log(props);
+
+    
     const date=new Date(props.data.data_time);
     date.setDate(date.getDate()-1)
-    //console.log(date);
+    
     const day=date.getDate();
     const month=date.getMonth();
     const year=date.getFullYear();
-    //console.log(day,month,year);
     //console.log("The current month is " + monthNames[month]);
     //const title=props.data.title;
     //console.log(title);
@@ -22,13 +35,11 @@ const Post=(props)=>{
     return(
         <div className='App-item'>
         <div className='row'>
-        <div className='col-4'>
-            <p className='App-date'><span id='specialDate'>{day}th </span> <br></br>{monthNames[month]} {year}</p>
-        </div>
-            <div className='col-4'>
+            <div className='col-6'>
+            <p className='App-date'><span id='specialDate'>{day}th </span> <br></br><span id='monthYear'>{monthNames[month]} {year} </span></p>
             <img src={props.data.assets[0].image_url} className='App-image'/>
         </div>
-            <div className='col-4'>
+            <div className='col-6'>
             <h2 className='App-title'>{props.data.title}</h2>
             <p className='App-body'>{props.data.body}</p>
         </div>
